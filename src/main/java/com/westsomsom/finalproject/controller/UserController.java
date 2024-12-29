f7c2d2a06f47b2e8458db071777e2102c48fdcfd
milkaisgoody <ehwhgod0712@gmail.com>
2024-12-29 12:44:43 +0900

    feat: mypage에 사용자 정보 수정기능 추가

--- src/main/java/com/westsomsom/finalproject/controller/UserController.java
+++ src/main/java/com/westsomsom/finalproject/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequiredArgsConstructor
 public class UserController {
+    private final MemberRepository memberRepository; // 유저정보 수정에 필요해서 추가
 
     @GetMapping("/mypage")
     public String getMyPage(HttpSession session, Model model) {
@@ -26,6 +27,34 @@ public class UserController {
         // 사용자 정보를 모델에 추가
         model.addAttribute("member", member);
         return "mypage"; // 마이페이지 뷰로 이동
+    }
+
+    //마이페이지 유저정보 수정 (카카오에서 가져온 정보를 제외한 성별, 연령대, 거주지에 대한 내용에 대해서만 수정가능)
+    @PutMapping("/mypage/update")
+    public ResponseEntity<MsgEntity> updateUserInfo(
+            @RequestParam("gender") Integer gender,
+            @RequestParam("ageGroup") String ageGroup,
+            @RequestParam("residence") String residence,
+            HttpSession session) {
+
+        // Get 로그인된 유저의 세션
+        Member member = (Member) session.getAttribute("member");
+        if (member == null) {
+            return ResponseEntity.badRequest().body(new MsgEntity("No logged-in user found", null));
+        }
+
+        // 유저정보 업데이트
+        member.setGender(gender);
+        member.setAgeGroup(ageGroup);
+        member.setResidence(residence);
+
+        // 변경된 내용 데이터베이스에 저장
+        memberRepository.save(member);
+
+        // 세션 객체 업데이트
+        session.setAttribute("member", member);
+
+        return ResponseEntity.ok(new MsgEntity("User information updated successfully", member));
     }
 
 }
